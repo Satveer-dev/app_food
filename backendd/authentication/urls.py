650cb6f05a53508661026465923a647c21a185df
Satveer-dev <163439778+Satveer-dev@users.noreply.github.com>
2024-05-04 16:55:54 +0530

    Delete backendd directory

--- backendd/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.HelloAuthView.as_view(),name='hello_auth'),
-    path('signup/',views.UserCreationView.as_view(),name='sign_up'),
-]
